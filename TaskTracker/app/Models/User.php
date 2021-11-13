c9bcb8e26771d1b951744946348160c251da0054
fman42 <igrok5554545@gmail.com>
2021-11-13 23:37:24 +0300

    Update user model

--- TaskTracker/app/Models/User.php
+++ TaskTracker/app/Models/User.php
@@ -12,12 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    private $roles = [
-        2 => 'employee',
-        1 => 'manager',
-        0 => 'admin'
-    ];
-
     public $timestamps = false;
 
     /**
@@ -49,9 +43,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function getRoleIdAttribute()
-    {
-        return $this->roles[$this->attributes['role_id']] ?? $this->roles[0];
-    }
 }
