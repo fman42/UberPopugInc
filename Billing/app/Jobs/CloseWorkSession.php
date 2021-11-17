ae0214bb3d4556341e621ff3644192990a4648d2
Milan <igrok5554545@gmail.com>
2021-11-17 10:58:47 +0300

    fix

--- Billing/app/Jobs/CloseWorkSession.php
+++ Billing/app/Jobs/CloseWorkSession.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
 use App\Models\User;
 
 class CloseWorkSession implements ShouldQueue
